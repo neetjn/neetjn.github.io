eaf70669c51a678f85a0cae824372c963622a45f
John Nolette <jnolette@transparent.com>
2019-07-31 20:04:03 -0400

    Adding contact component

--- src/containers/Portfolio.svelte
+++ src/containers/Portfolio.svelte
@@ -1,11 +1,13 @@
 <script>
   import Announcement from './Announcement.svelte'
   import Carousel from './Carousel.svelte'
+  import Contact from './Contact.svelte'
   import Footer from './Footer.svelte'
 </script>
 
 <Announcement />
 <div class="flex flex-col h-full">
   <Carousel />
+  <Contact />
   <Footer />
 </div>
