ebb2d14aaf86109514727866700b392a14812b0f
John Nolette <jnolette@transparent.com>
2019-08-07 20:44:54 -0400

    Updating build

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,1 +1,3 @@
 <h1>Footer</h1>
+
+Photo by <a href="https://www.pexels.com/@picjumbo-com-55570?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">picjumbo.com</a> from <a href="https://pexels.com">Pexels</a>
